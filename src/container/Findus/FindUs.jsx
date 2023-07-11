6f0d3ccf1e2684a62555183b12efe749da2bafea
Monishka Jha <jhamonishka@gmail.com>
2023-07-11 22:13:28 +0530

    Updated the Project

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding ' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title={'Contact'} />
+      <h1 className='headtext__cormorant' style={{marginBottom : '3rem'}}>Find Us</h1>
+      <div className="app__wrapper_content">
+        <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+      </div>
+      
+
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus}/>
+    </div>
   </div>
 );
 
