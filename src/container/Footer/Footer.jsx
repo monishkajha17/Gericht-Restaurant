0ddb71af59d34c54b83419ba953c8a4067572629
Monishka Jha <jhamonishka@gmail.com>
2023-07-11 23:54:51 +0530

    Updating Project

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 
+import { FooterOverlay,Newsletter } from '../../components';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
+
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay/>
+    <Newsletter/>
+
+    <div className="app__footer-links">
+    <div className="app__footer-links_contact">
+      <h1 className='app__footer-headtext'>Contact Us</h1>
+      <p className='p__opensans'>9 W 53rd St, New York, NY 10019, USA</p>
+      <p className='p__opensans'>+1 212-344-1230</p>
+      <p className='p__opensans'>+1 212-344-1230</p>
+
+    </div>
+
+    <div className="app__footer-links_logo">
+      <img src={images.gericht} alt='footer-logo' />
+      <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others.”</p>
+      <img src={images.spoon } alt='sppon' style={{marginTop: 15}} />
+
+
+    </div>
+
+    <div className="app__footer-links_work">
+
+    </div>
+
+    </div>
+
+    <div className="footer__copyright">
+      <p className='p__opensans'>2023 Gerícht. All Rights reserved.</p>
+    </div>
+    
   </div>
 );
 
